d9069787206e5c3a3a8c6bd32ae189d3141a3044
Prosperity-Mthethwa <nyamboseprosperity@gmail.com>
2024-09-11 14:39:28 +0200

    added remaining preferances

--- index.js
+++ index.js
@@ -28,6 +28,9 @@ const songs = [
 const guardians = {
     "Star-Lord": "Rock",
     "Gamora": "Pop",
+    "Drax": "R&B", 
+    "Rocket": "Country",
+    "Groot": "Indie", 
     // Add preferences for Drax, Rocket, and Groot
 };
 
